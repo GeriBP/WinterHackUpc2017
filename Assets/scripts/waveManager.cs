45a1e5ba5521d07c66525e9e96cefedffb6998eb
GeriBP <gbelenguer@gmail.com>
2017-10-14 21:42:57 +0200

    wave manager+navmesh in main

---
+++ Assets/scripts/waveManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveManager : MonoBehaviour {
+    public float initial, incTime, minTime, increment;
+    public GameObject slime, ghost;
+    public GameObject[] spawnpointSlime;
+    //fantasmes only initialize
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(spawnGuys());
+        StartCoroutine(incTimer());
+    }
+
+    IEnumerator spawnGuys()
+    {
+        while (true)
+        {
+            Instantiate(enemy, //POSICIO, radiusF), Quaternion.identity);
+            yield return new WaitForSeconds(iniTime);
+        }
+    }
+
+    IEnumerator incTimer()
+    {
+        while (iniTime >= minTime)
+        {
+            iniTime -= increment;
+            yield return new WaitForSeconds(incTime);
+        }
+        yield return null;
+    }
+}
