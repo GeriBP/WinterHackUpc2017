c7c2b8f69247158b4e917735956e9da278dfc14d
GeriBP <gbelenguer@gmail.com>
2017-10-14 17:44:32 +0200

    Preparing enemies

---
+++ Assets/scripts/Ghost.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour {
+    #region parameters
+    [Header("Ghost numeric parameters")]
+    [SerializeField]
+    float hp;
+    private float currrentHp;
+    #endregion
+    // Use this for initialization
+    void Start () {
+        currrentHp = hp;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
