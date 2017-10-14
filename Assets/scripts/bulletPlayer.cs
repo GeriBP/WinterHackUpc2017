4adee99fbafc478a4a23d5b43f09b9c290b7a028
Davigox <thedavigo95@gmail.com>
2017-10-15 00:56:07 +0200

    Bullet killnation!

---
+++ Assets/scripts/bulletPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletPlayer : MonoBehaviour {
+    [HideInInspector]
+    public float damage;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "enemy")
+        {
+            other.gameObject.SendMessage("TakeDamage",damage);
+        }
+        //Instantiate PS
+        Destroy(gameObject);
+    }
+}
