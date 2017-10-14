8e968031e0db29f0e481171cc96b9f12a8069f70
GeriBP <gbelenguer@gmail.com>
2017-10-14 12:06:41 +0200

    Added myo scene

---
+++ Assets/scripts/PlayerWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    #region parameters
+    [Header("Player numeric parameters")]
+    [SerializeField]
+    float hp;
+    private float currentHp;
+    [Header("Weapon numeric parameters")]
+    [SerializeField]
+    float firerate;
+    #endregion
+    // Use this for initialization
+    void Start()
+    {
+        currentHp = hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
