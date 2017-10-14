04806dd5380d143e3ad625f6cd767d85c33e41fb
mnxanl <mnxanl@github.com>
2017-10-14 20:53:54 +0200

    Navigator Mesh iN!

--- Assets/scripts/FindDest.cs
+++ Assets/scripts/FindDest.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class FindDest : MonoBehaviour {
 
-	public Transform portal_1;
-	public Transform portal_2;
+	public GameObject portal_1;
+	public GameObject portal_2;
 	private Vector3 target;
 
 	// Use this for initialization
 	void Start () {
+		portal_1 = GameObject.Find("Stone_Gate");
+		portal_2 = GameObject.Find("Stone_Gate2");
 		float rand = Random.Range(0.0f, 2.0f);
-		if (rand <= 1.0f) target = portal_1.position;
-		else target = portal_2.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {		
+		if (rand <= 1.0f) target = portal_1.transform.position;
+		else target = portal_2.transform.position;
+		transform.
 		transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = target;
 	}
 }
