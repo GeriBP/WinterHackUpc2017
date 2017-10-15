9482e9c6b169cfed200a7f27f29914f25a087a6e
GeriBP <gbelenguer@gmail.com>
2017-10-15 07:30:53 +0200

    no message

---
+++ Assets/scripts/menuCode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using UnityEngine.UI;
+public class menuCode : MonoBehaviour {
+    private ThalmicMyo myo;
+    [SerializeField]
+    GameObject myoManger;
+    // Use this for initialization
+    void Start () {
+        //Get myo controller
+        myo = myoManger.GetComponent<ThalmicMyo>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (myo.pose == Pose.Fist && Time.time > 5.0f)
+        {
+            go();
+        }
+    }
+
+    private void go()
+    {
+        SceneManager.LoadScene("tutorial");
+    }
+}
---
+++ Assets/scripts/tutorialManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class tutorialManager : MonoBehaviour {
+    public Sprite[] s;
+    public Image image;
+    public float t;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(slides());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator slides()
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            image.sprite = s[i];
+            yield return new WaitForSeconds(t);
+        }
+        SceneManager.LoadScene("main");
+        yield return null;
+    }
+}
